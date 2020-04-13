19c1a6687cf9a1906ec32982ec2e63c97397b8d1
Felix <felix-sievers@freenet.de>
2020-04-13 16:10:49 +0200

    test on where to put the comments

--- myClass.cpp
+++ myClass.cpp
@@ -1,9 +1,15 @@
 #include "myClass.h"
 #include <iostream>
+/**
+ * constructor definition in cpp
+ */
 myClass::myClass() {
 	std::cout << "constructor" << std::endl;
 }
 
+/**
+ * my function definiton in cpp
+ */
 myClass::myfunction() {
 	std::cout << "myfunc" << std::endl;
 }
--- myClass.h
+++ myClass.h
@@ -1,9 +1,18 @@
+/**
+ * this is the class description of my class
+ */
 class myClass {
 	private:
-		int n;
-		double k;
-		double* p;
+		int n;		/**< integer n for something */
+		double k;	/**< maybe stiffness */
+		double* p; 	/**< could be a density */
 	public:
+		/**
+		 * constructor of myClass in .h
+		 */
 		myClass();
+		/**
+		 * declaration of myfunction
+		 */
 		myfunction();
 };
