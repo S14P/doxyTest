7d4fde841465b305531e26e5f65a67dfa8ee727d
Travis CI <travis@travis-ci.org>
2020-04-13 14:12:28 +0000

    Deploy code docs to GitHub Pages Travis build: 17
    Commit: 19c1a6687cf9a1906ec32982ec2e63c97397b8d1

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['myclass',['myClass',['../classmy_class.html',1,'']]]
+  ['myclass',['myClass',['../classmy_class.html',1,'myClass'],['../classmy_class.html#aa9e6b37bebb590a0d6aebdb165e308bc',1,'myClass::myClass()']]],
+  ['myfunction',['myfunction',['../classmy_class.html#a14f71b961aa14a651c1838ad950218dc',1,'myClass']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -2,20 +2,23 @@ var indexSectionsWithContent =
 {
   0: "dm",
   1: "m",
-  2: "d"
+  2: "m",
+  3: "d"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "pages"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Pages"
+  2: "Functions",
+  3: "Pages"
 };
 
